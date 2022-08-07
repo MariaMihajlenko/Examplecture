9c0afefcb8d888837bd39525a10013b5f21abe49
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-07 20:07:19 +0500

    base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int f = 4;
+int d = 8;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (f > max ) max = f;
+if (d > max ) max = d;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
